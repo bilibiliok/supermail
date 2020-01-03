e134e609ea8b5a7572e3c283bce6414dd0620be2
bilibiliok <1831631787@qq.com>
2020-01-03 22:31:49 +0800

    feat

---
+++ .history/src/views/profile/Profile_20200103150908.vue
@@ -0,0 +1,44 @@
+<template>
+	<div id="profile">
+		<nar-bar class="nav-bar">
+			<div slot="center">小马哥商城</div>
+		</nar-bar>
+		<user-info></user-info>
+		<section class="account">
+			<div class="account-item">
+				<div class="number">
+					<span>0.00</span>元
+				</div>
+				<div class="account-info">
+					我的
+				</div>
+			</div>
+		</section>
+	</div>
+</template>
+<script>
+import NarBar from 'components/common/navbar/NavBar'
+import UserInfo from './childComps/UserInfo'
+export default {
+	data() {
+		return {
+
+		}
+	},
+	components:{
+		NarBar,
+		UserInfo
+	}
+}
+</script>
+<style  scoped>
+	#profile {
+		background-color: #f2f2f2;
+	}
+
+	.nav-bar {
+		background-color: var(--color-tint);
+		font-weight: 700;
+		color: #fff;
+	}
+</style>
